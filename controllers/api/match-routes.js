775cb1c4639add9d954ed5445c0ce2a3e5c83a64
winglovecola@gmail.com <winglovecola@gmail.com>
2023-03-03 05:33:48 -0500

    updated chat function, layout, css

--- controllers/api/match-routes.js
+++ controllers/api/match-routes.js
@@ -6,26 +6,71 @@ const { User, Match } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     const userLikeData = await Match.findAll({
-      include: [{ model: User }],
+      attributes: ['userid', 'match_id'],
       where: {
         userid: req.session.userid,
       },
     });
-    const userLike = userLikeData.get({ plain: true });
+
+    const userLike = userLikeData.map((thisUserLikeData) =>
+      thisUserLikeData.get({ plain: true })
+    );
+
+    let userLikeList = [];
+    userLike.forEach((element) => {
+      userLikeList.push (element.match_id);
+    });
+
+    console.log (userLikeList);
+
+
 
     const userLikeMeData = await Match.findAll({
-      include: [{ model: User }],
+      attributes: ['userid', 'match_id'],
       where: {
         match_id: req.session.userid,
       },
     });
-    const userLikeMe = userLikeMeData.get({ plain: true });
+
+    const userLikeMe = userLikeMeData.map((thisUserLikeData) =>
+      thisUserLikeData.get({ plain: true })
+    );
+
+    let userLikeMeList = [];
+    userLikeMe.forEach((element) => {
+      userLikeMeList.push (element.userid);
+    });
+
+    console.log (userLikeMeList);
 
 
-    const userMatch = {like: userLike, likeMe: userLikeMe};
-
-
+    let userMatchedList = [];
+    userLikeList.forEach((element) => {
+      if (userLikeMeList.includes (element)) {
+        userMatchedList.push (element);
+      }
+    });
+    console.log (userMatchedList);
     //console.log('req.session.userid:', matchDataPlain);
+
+
+    console.log (userLikeList.concat(userLikeMeList));
+
+    const allUserList = userLikeList.concat(userLikeMeList);
+
+    const userDataObj = await User.findAll({
+      attributes: ['id', 'username', 'firstname', 'lastname', 'sex', 'interest', 'avatar', 'avatar_type'],
+      where: {
+        id: allUserList,
+      },
+    });
+
+    const userData = userDataObj.map((thisUserData) =>
+      thisUserData.get({ plain: true })
+    );
+
+
+    const userMatch = {liked: userLikeList, likedMe: userLikeMeList, matched: userMatchedList, userData: userData};
 
     res.status(200).json(userMatch);
 
